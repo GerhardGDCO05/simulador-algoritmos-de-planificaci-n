b641c3dfdd9ebd782d69d0e9432d42d5a2372bc9
GerhardGDCO05 <gerhardcastro8a18@gmail.com>
2025-12-02 10:31:11 -0400

    Inicio limpio: Proyecto Simulador de Algoritmos de Planificación

---
+++ Back-simulator/src/test/java/com/example/Back_simulator/BackSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Back_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
